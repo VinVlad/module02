33c73e368cf72b3379c36e942462680c30151de9
Vinokurov Vladislav <vinokurovvlad.26@gmail.com>
2024-01-17 15:17:20 +1000

    finish task 02_05 searching max int with functions

--- functions/main.go
+++ functions/main.go
@@ -12,10 +12,22 @@ func contains(a []string, x string) bool {
 	return false
 }
 
+func getMax(a ...int) int {
+	x := 0
+	for _, v := range a {
+		if v > x {
+			x = v
+		}
+	}
+	return x
+}
+
 func main() {
 
 	a := []string{"cлово", "буква", "число"}
 	x := "число"
 
 	fmt.Println(contains(a, x))
+
+	fmt.Println(getMax(1, 2, 7, 5, 9, 4, -2, 2, 3))
 }
